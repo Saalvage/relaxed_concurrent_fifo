7429b63a439a2540f3483a9ad66262ceb81645b3
Salvage <29021710+Saalvage@users.noreply.github.com>
2024-05-07 18:11:06 +0200

    Initial commit

---
+++ relaxed_concurrent_fifo/relaxed_concurrent_fifo.cpp
@@ -0,0 +1,52 @@
+﻿#include "relaxed_concurrent_fifo.h"
+
+#include <ranges>
+#include <cassert>
+
+static constexpr int COUNT = 512;
+
+template <template <typename, size_t> typename T>
+void test_full_capacity() {
+	T<int, COUNT + 1> buf;
+	for (int i : std::views::iota(0, COUNT)) {
+		buf.push(i);
+	}
+	for (int i : std::views::iota(0, COUNT)) {
+		assert(buf.pop() == i);
+	}
+}
+
+template <template <typename, size_t> typename T>
+void test_single_element() {
+	T<int, COUNT + 1> buf;
+	for (int i : std::views::iota(0, COUNT * 10)) {
+		buf.push(i);
+		assert(buf.pop() == i);
+	}
+}
+
+template <template <typename, size_t> typename T>
+void test_empty_pop() {
+	T<int, COUNT + 1> buf;
+	assert(!buf.pop().has_value());
+	buf.push(1);
+	buf.pop();
+	assert(!buf.pop().has_value());
+	for (int i : std::views::iota(0, COUNT * 10)) {
+		buf.push(i);
+		buf.pop();
+	}
+	assert(!buf.pop().has_value());
+}
+
+template <template <typename, size_t> typename T>
+void test_all() {
+	test_full_capacity<T>();
+	test_single_element<T>();
+	test_empty_pop<T>();
+}
+
+int main() {
+	test_all<circular_buffer>();
+	return 0;
+}
---
+++ relaxed_concurrent_fifo/relaxed_concurrent_fifo.h
@@ -0,0 +1,37 @@
+﻿#ifndef RELAXED_CONCURRENT_FIFO_H_INCLUDED
+#define RELAXED_CONCURRENT_FIFO_H_INCLUDED
+
+#include <utility>
+#include <optional>
+
+// TODO: How should a full queue be handled?
+// Currently popping from a full queue is not possible.
+template <typename T, size_t SIZE>
+class circular_buffer {
+private:
+	T buffer[SIZE];
+
+	size_t head = 0;
+	size_t tail = 0;
+
+	inline size_t next(size_t curr) {
+		return (curr + 1) % SIZE;
+	}
+
+public:
+	void push(T t) {
+		head = next(head);
+		buffer[head] = std::move(t);
+	}
+
+	std::optional<T> pop() {
+		if (head == tail) {
+			return std::nullopt;
+		}
+
+		tail = next(tail);
+		return std::move(buffer[tail]);
+	}
+};
+
+#endif // RELAXED_CONCURRENT_FIFO_H_INCLUDED
