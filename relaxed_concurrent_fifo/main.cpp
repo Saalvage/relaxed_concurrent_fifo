377b5734ec7f1250d0db0e4e57769d716bad308e
Salvage <29021710+Saalvage@users.noreply.github.com>
2024-08-20 19:43:37 +0200

    Consistency test

--- relaxed_concurrent_fifo/main.cpp
+++ relaxed_concurrent_fifo/main.cpp
@@ -8,6 +8,7 @@
 #include <cassert>
 #include <filesystem>
 #include <fstream>
+#include <unordered_set>
 #include <iostream>
 
 static constexpr int COUNT = 512;
@@ -61,7 +62,71 @@ void test_all() {
 	test_full_push<T>();
 }
 
+template <size_t THREAD_COUNT, size_t FIFO_SIZE, uint32_t PER_THREAD_ELEMENTS>
+void test_consistency() {
+	relaxed_fifo<uint64_t, THREAD_COUNT> fifo{FIFO_SIZE};
+	auto handle = fifo.get_handle();
+
+	constexpr int pre_push = FIFO_SIZE / 2;
+	std::unordered_multiset<uint64_t> test_ints;
+	for (size_t index = 0; index < pre_push; index++) {
+		auto i = index | (1ull << 63);
+		handle.push(i);
+		test_ints.emplace(i);
+	}
+
+	std::barrier a{ (ptrdiff_t)(THREAD_COUNT + 1) };
+	std::vector<std::jthread> threads(THREAD_COUNT);
+	std::vector<std::vector<uint64_t>> test(THREAD_COUNT);
+	std::vector<std::vector<uint64_t>> popped(THREAD_COUNT);
+	for (size_t i = 0; i < THREAD_COUNT; i++) {
+		threads[i] = std::jthread([&, i]() {
+			size_t its = 0;
+			auto handle = fifo.get_handle();
+			a.arrive_and_wait();
+			for (uint64_t j = 0; j < PER_THREAD_ELEMENTS; j++) {
+				auto val = (i << 32) | j;
+				test[i].push_back(val);
+				while (!handle.push(val)) {}
+				std::optional<uint64_t> pop;
+				do {
+					pop = handle.pop();
+				} while (!pop.has_value());
+				popped[i].push_back(pop.value());
+			}
+		});
+	}
+	a.arrive_and_wait();
+	for (auto& thread : threads) {
+		thread.join();
+	}
+
+	std::unordered_multiset<uint64_t> popped_ints;
+	for (size_t index = 0; index < pre_push; index++) {
+		popped_ints.emplace(handle.pop().value());
+	}
+
+	for (size_t i = 0; i < THREAD_COUNT; i++) {
+		for (auto i : popped[i]) {
+			popped_ints.emplace(i);
+		}
+		for (auto i : test[i]) {
+			test_ints.emplace(i);
+		}
+	}
+
+	if (handle.pop().has_value()) {
+		throw std::runtime_error("Invalid element left!");
+	}
+
+	if (popped_ints != test_ints) {
+		throw std::runtime_error("Sets did not match!");
+	}
+}
+
 int main() {
+	test_consistency<8, 1 << 12, 1 << 20>();
+
 	namespace fs = std::filesystem;
 
 	constexpr const char* format = "fifo-data-{}-{:%FT%H-%M-%S}.csv";
