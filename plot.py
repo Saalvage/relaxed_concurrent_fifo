0450d6a9d044e3631265c4ad3491637564f4a160
Salvage <29021710+Saalvage@users.noreply.github.com>
2024-10-19 21:17:56 +0200

    Improve plotting script again

--- plot.py
+++ plot.py
@@ -17,6 +17,8 @@ file = sys.argv[1] if len(sys.argv) == 2 else input("Please enter the .csv data 
 with open(file) as file:
     lines = csv.reader(file)
     for row in lines:
+        if len(row) < 3:
+            continue
         name = row[0]
         if name not in impls:
             impls[name] = impl()
@@ -30,10 +32,7 @@ with open(file) as file:
 for k, v in impls.items():
     values = v.values.values()
     avgs = list(map(statistics.mean, values))
-    std = list(map(statistics.stdev, values)) if len(first(values)) > 1 else [0] * len(first(values))
-    print(v.values.keys())
-    print(avgs)
-    print(std)
+    std = list(map(statistics.stdev, values)) if len(first(values)) > 1 else ([0] * len(values))
     xs, ys, std = zip(*sorted(zip(v.values.keys(), avgs, std)))
     plt.errorbar(xs, ys, yerr=std, label=k, fmt="-o", capsize=3, ecolor="black")
     plt.xlabel("Processors")
