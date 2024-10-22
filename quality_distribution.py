9871c010c953738c2683dab89a88d393843ad791
Salvage <29021710+Saalvage@users.noreply.github.com>
2024-10-22 22:19:11 +0200

    Limit data in quality distribution

--- quality_distribution.py
+++ quality_distribution.py
@@ -13,20 +13,28 @@ file = sys.argv[1] if len(sys.argv) > 1 else input("Please enter the .csv data f
 
 def compute(values, out):
     res = list(sorted(map(lambda x: tuple(map(int, x.split(';'))), values.split('|')[:-1])))
-    cum = [None] * len(res)
     total = sum(map(lambda x: x[1], res))
     running_total = 0
     out.write("err cum\n")
+    goal = 1
     for i in range(len(res)):
-        out.write(str(res[i][0]) + " " + str(float(total - running_total) / total) + "\n")
-        cum[i] = float(total - running_total) / total
+        curr = float(total - running_total) / total
+        if curr <= goal:
+            out.write(str(res[i][0]) + " " + str(float(total - running_total) / total) + "\n")
+            goal -= 0.01
         running_total += res[i][1]
+        # Consciously ignoring the last 1%
     out.write("\n")
+
+done = { }
 
 with open(file) as file:
     lines = csv.reader(file)
     for row in lines:
-        with open(row[0] + "_rank_error.txt", "w") as out:
-            compute(row[5], out)
-        with open(row[0] + "_delay.txt", "w") as out:
-            compute(row[5], out)
+        # We have enough data as-is, only use a single testrun
+        if row[0] not in done:
+            with open(row[0] + "_rank_error.txt", "w") as out:
+                compute(row[5], out)
+            with open(row[0] + "_delay.txt", "w") as out:
+                compute(row[9], out)
+            done[row[0]] = None
